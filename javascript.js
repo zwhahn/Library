20fbb54881f56eeb7e68ca77d60d9b92418f43fa
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-12-07 21:35:47 -0800

    Add addBookToScreen function

--- javascript.js
+++ javascript.js
@@ -6,25 +6,27 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+const cardContainer = document.querySelector(".card-container");
 
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
+    addBookToScreen(book)
 }
 
 addBookToLibrary('Six Seasons', 'Joshua Mcfadden', 423, true);
 addBookToLibrary('French Patisserie', 'Ferrandi', 674, true);
-addBookToLibrary("What's For Dessert", 'Claire Saffitz', 267, false);
-addBookToLibrary('The art of French Cooking', 'Julia Child', 232, false);
-addBookToLibrary('Beard on Bread', 'James Beard', 345, false);
+addBookToLibrary("What's For Dessert", 'Claire Saffitz', 267, undefined);
+addBookToLibrary('The art of French Cooking', 'Julia Child', 232, undefined);
+addBookToLibrary('Beard on Bread', 'James Beard', 345, true);
 console.log(myLibrary);
 
-const container = document.querySelector(".container");
 
-for (const book of myLibrary) {
+function addBookToScreen(book) {
     const card = document.createElement("div")
+    card.classList.add("card")
     card.textContent = book.title;
-    container.appendChild(card);
+    cardContainer.appendChild(card);
 }
 
 const dialog = document.querySelector("dialog")
@@ -39,8 +41,8 @@ const form = document.getElementById("new-book-form");
 
 function getData(form) {
     var formData = new FormData(form);
-
-    console.log(Object.fromEntries(formData));
+    formDataObject = Object.fromEntries(formData);
+    addBookToLibrary(formDataObject.title, formDataObject.author, formDataObject.pages, formDataObject.read)
 }
 
 addToLibraryBtn.addEventListener("click", function (e) {
